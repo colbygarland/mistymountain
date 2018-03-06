dba02d2a916ba1182deaaf4ebe87033340d6b695
Colby Garland <colbygarland@gmail.com>
2018-03-05 20:25:06 -0700

    added parent menu opening in mobile

--- js/main.js
+++ js/main.js
@@ -2,7 +2,19 @@ jQuery(window).bind("load", function() {
   'use strict';
   jQuery(function($) {
 
+    // for lightbox for pictures
     $('.venobox').venobox();
+
+    //Double Click to open parent menu item in mobile
+    $('ul.mobile-menu>li>a').click(function(e){
+      if( $(this).hasClass('go') || $(this).next('ul.children').length < 1 ){
+        return;
+      }else{
+        e.preventDefault();
+        $(this).addClass('go');
+      }
+    });
+
 
   });
 });
