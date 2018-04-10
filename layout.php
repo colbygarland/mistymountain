0b99652e85a54dd67558ddfcb0fc87f2496e2862
Colby Garland <colbygarland@gmail.com>
2018-04-09 20:08:06 -0600

    added custom block padding

--- layout.php
+++ layout.php
@@ -165,11 +165,10 @@ add_filter( 'flexible_columns_wrap_outer_end', 'IDP_Column_WrapOuter_End');
 function IDP_Column_WrapOuter(){
 
   $contain = '';
-  $padding = get_field('block_padding');
 
 	if ( get_row_layout() == 'full_width_row' || get_row_layout() == '2_column_row' || get_row_layout() == '3_column_row' || get_row_layout() == '4_column_row' ){
 
-	  $padding = get_field('block_padding');
+	  $padding = get_sub_field('block_padding');
     
     if( have_rows('column') || have_rows('column_1') || have_rows('column_2') || have_rows('column_3') || have_rows('column_4')):
     
