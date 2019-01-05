1877898514fd4115be091a76172302ff42736b1a
Colby Garland <colbygarland@gmail.com>
2019-01-05 09:03:00 -0700

    Fixed favicon path

--- header.php
+++ header.php
@@ -33,11 +33,11 @@ endif;
     gtag('config', 'UA-113745102-1');
 </script>
 
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#756960">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri();?>/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri();?>/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri();?>/favicon-16x16.png">
+<link rel="manifest" href="<?php echo get_template_directory_uri();?>/site.webmanifest">
+<link rel="mask-icon" href="<?php echo get_template_directory_uri();?>/safari-pinned-tab.svg" color="#756960">
 <meta name="msapplication-TileColor" content="#756960">
 <meta name="theme-color" content="#756960">
 
